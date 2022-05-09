ce4e290be492305e3eeb6c0765b4eef9f067cb1d
Max Milton <max@happycode.ltd>
2022-05-09 16:41:07 +1000

    Add comment

--- packages/trackx-cli/src/types.ts
+++ packages/trackx-cli/src/types.ts
@@ -1,5 +1,6 @@
 import type * as mri from 'mri';
 
+// XXX: mri is the underlying CLI parser used by sade
 export interface GlobalOptions extends mri.Argv {
   /** File path to TrackX API config. */
   config: string;
