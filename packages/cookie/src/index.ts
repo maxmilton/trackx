55d6f43629d373a2797f07fb5afb3f42e6ba6a52
Max Milton <max@happyhackers.xyz>
2022-12-22 14:39:08 +1100

    chore: More cookie improvements, including validation

--- packages/cookie/src/index.ts
+++ packages/cookie/src/index.ts
@@ -1,5 +1,16 @@
 interface CookieInit {
+  /**
+   * The name of the cookie.
+   */
   key: string;
+  /**
+   * The value of the cookie.
+   *
+   * This is not encoded or decoded in any way. It is passed through as-is. The
+   * caller is responsible for encoding and decoding the value.
+   *
+   * Do not wrap the value in quotes.
+   */
   value: string;
   /**
    * The date when the cookie will expire. When undefined, the cookie will
@@ -11,26 +22,40 @@ interface CookieInit {
   /** @deprecated Use `expires` instead. */
   maxAge?: never;
   /**
+   * The domain for which the cookie is valid.
+   *
    * @default undefined
    */
   domain?: string | undefined;
   /**
+   * The path for which the cookie is valid.
+   *
    * @default undefined
    */
   path?: string | undefined;
   /**
+   * Should the cookie only be accessible via HTTP(S)? This prevents the cookie
+   * from being accessed via JavaScript.
+   *
    * @default true
    */
   httpOnly?: boolean | undefined;
   /**
+   * Should the cookie only be sent over HTTPS? Note that this is required for
+   * `SameSite=None` cookies.
+   *
+   * @see https://web.dev/samesite-cookies-explained/#samesitenone-must-be-secure
+   *
    * @default true
    */
   secure?: boolean | undefined;
   /**
-   * @default 'Strict'
+   * The `SameSite` attribute of the cookie.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
    * @see https://web.dev/samesite-cookies-explained/
+   *
+   * @default 'Strict'
    */
   sameSite?: 'Lax' | 'Strict' | 'None' | undefined;
 }
@@ -65,8 +90,19 @@ export class Cookie {
     return new Cookie({ key, value });
   }
 
+  /**
+   * The name of the cookie.
+   */
   declare key: string;
 
+  /**
+   * The value of the cookie.
+   *
+   * This is not encoded or decoded in any way. It is passed through as-is. The
+   * caller is responsible for encoding and decoding the value.
+   *
+   * Do not wrap the value in quotes.
+   */
   declare value: string;
 
   /**
@@ -75,20 +111,50 @@ export class Cookie {
    */
   declare expires: Date | undefined;
 
+  /** @deprecated Use `expires` instead. */
+  declare maxAge: never;
+
+  /**
+   * The domain for which the cookie is valid.
+   */
   declare domain: string | undefined;
 
+  /**
+   * The path for which the cookie is valid.
+   */
   declare path: string | undefined;
 
+  /**
+   * Should the cookie only be sent over HTTPS? Note that this is required for
+   * `SameSite=None` cookies.
+   *
+   * @see https://web.dev/samesite-cookies-explained/#samesitenone-must-be-secure
+   */
   declare secure: boolean | undefined;
 
+  /**
+   * Should the cookie only be accessible via HTTP(S)? This prevents the cookie
+   * from being accessed via JavaScript.
+   */
   declare httpOnly: boolean | undefined;
 
   /**
+   * The `SameSite` attribute of the cookie.
+   *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
    * @see https://web.dev/samesite-cookies-explained/
    */
   declare sameSite: 'Lax' | 'Strict' | 'None';
 
+  /**
+   * Create a new Cookie.
+   *
+   * Note that validation is only performed when `NODE_ENV` is set to
+   * `development`. No validation is performed in production for performance.
+   *
+   * Neither the `key` nor `value` are encoded or decoded. They are passed
+   * through as-is so that the caller can decide how to encode them.
+   */
   constructor(options: CookieInit) {
     this.key = options.key;
     this.value = options.value;
@@ -98,6 +164,32 @@ export class Cookie {
     this.secure = options.secure ?? true;
     this.httpOnly = options.httpOnly ?? true;
     this.sameSite = options.sameSite || 'Strict';
+
+    if (process.env.NODE_ENV === 'development') {
+      // Check key and value contain only ASCII and no invalid characters
+      if (
+        !/^[\u0020-\u007E]*$/.test(this.key)
+        || /[\t "(),/:;<=>?@[\\\]{}]/.test(this.key)
+      ) {
+        throw new Error(`Cookie key contains invalid characters: ${this.key}`);
+      }
+      if (
+        !/^[\u0020-\u007E]*$/.test(this.value)
+        || /[\t "(),/:;<=>?@[\\\]{}]/.test(this.value)
+      ) {
+        throw new Error(
+          `Cookie value contains invalid characters: ${this.value}`,
+        );
+      }
+
+      if (options.maxAge) {
+        throw new Error('maxAge is not supported, use expires instead');
+      }
+
+      if (this.sameSite === 'None' && !this.secure) {
+        throw new Error('SameSite=None cookies must be Secure');
+      }
+    }
   }
 
   setExpires(date: Date): void {
